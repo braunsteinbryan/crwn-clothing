cda09d8aea1cd37f8a79b76988537c484a221aaa
braunsteinbryan <braunsteinbryan@gmail.com>
2021-04-02 08:34:13 -0400

    finished refactoring code to use createStructuredSelector from reselect

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -7,11 +7,16 @@ export const selectCartItems = createSelector(
     (cart) => cart.cartItems
 );
 
+export const selectCartHidden = createSelector(
+    [selectCart],
+    (cart) => cart.hidden
+)
+
 export const selectCartItemsCount = createSelector(
     [selectCartItems],
     cartItems => cartItems.reduce(
         (accumaltedQuantity, cartItem) => 
         accumaltedQuantity + cartItem.quantity, 
-    0
-)
+        0
+    )
 );
